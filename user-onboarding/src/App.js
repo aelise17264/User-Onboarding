40b24788e531192d4ddb747e191f419195ed2db7
unknown <aelise17264@gmail.com>
2020-08-19 13:32:11 -0600

    fleshing out app

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,12 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import * as yup from 'yup'
+
+const initialValues ={
+name: '',
+email: '',
+password: '',
+terms: false,
+
+}
+
+const initialErrors = {
+  name: '',
+  email: '',
+password: '',
+terms: '',
+
+}
+
+const initialUsers = []
+const initialDisabled = true
 
 function App() {
+
+const [users, setUsers] = useState(initialUsers)
+const [values, setValues] = useState(initialValues)
+const [errors, setErrors] = useState(initialErrors)
+const [disabled, setDisabled] = useState(initialDisabled)
+
+const thisUrl = 'https://reqres.in/api/users'
+
+const getUsers = () => {
+  axios.get(thisUrl)
+  .then(result => {
+    setUsers(result.data)
+  })
+  .catch(error => {
+    console.log('check the axios get')
+  })
+}
+
+const postUser = newUser => {
+  axios.post(thisUrl, newUser)
+  .then(result => {
+    setUsers([...users, result.data])
+  })
+  .catch(error => {
+    console.log('check the axios post')
+  })
+  .finally(() => {
+    setValues(initialValues)
+  })
+}
+
   return (
     <div className="App">
 
-      
+
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
